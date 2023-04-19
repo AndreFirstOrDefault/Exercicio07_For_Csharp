e4c86b040f78d24d233067a9456dd9e58c0dabff
Andre Luiz <andre.java.ads.2022@gmal.com>
2023-04-19 16:16:45 -0300

    Enviando Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioFor07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++) 
+            {
+                Console.Write(i + " ");
+                Console.Write(i*i + " ");
+                Console.Write(i*i*i + "\n");
+
+            }
+        }
+    }
+}
